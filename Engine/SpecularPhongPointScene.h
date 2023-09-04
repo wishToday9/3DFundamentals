911e86c4f1b1c4e7573d4e009e85985bbf758692
zhang <419720775@qq.com>
2023-09-04 23:05:56 +0800

    unable absolute value

--- Engine/SpecularPhongPointScene.h
+++ Engine/SpecularPhongPointScene.h
@@ -82,18 +82,26 @@ public:
 		}
 		if (kbd.KeyIsPressed('R'))
 		{
-			offset_z += 0.2f * dt;
+			offset_z += 0.7f * dt;
 		}
 		if (kbd.KeyIsPressed('F'))
 		{
-			offset_z -= 0.2f * dt;
+			offset_z -= 0.7f * dt;
+		}
+		if (kbd.KeyIsPressed('N'))
+		{
+			phi -= 1.8f * dt;
+		}
+		if (kbd.KeyIsPressed('M'))
+		{
+			phi += 1.8f * dt;
 		}
 	}
 	virtual void Draw() override
 	{
 		pipeline.BeginFrame();
 
-		const auto proj = Mat4::Projection(2.0f, 2.0f, 1.0f, 10.0f);
+		const auto proj = Mat4::ProjectionHFOV(100.0f, 1.33333f, 0.5f, 4.0f);
 
 		// set pipeline transform
 
@@ -101,11 +109,12 @@ public:
 			Mat4::RotationX(theta_x) *
 			Mat4::RotationY(theta_y) *
 			Mat4::RotationZ(theta_z) *
-			Mat4::Translation(0.0f, 0.0f, offset_z)
+			Mat4::Translation(0.0f, 0.0f, offset_z) *
+			Mat4::RotationY(phi)
 		);
 		pipeline.effect.vs.BindProjection(proj);
 
-		pipeline.effect.ps.SetLightPosition({ lpos_x,lpos_y,lpos_z });
+		pipeline.effect.ps.SetLightPosition({ lpos_x, lpos_y, lpos_z } );
 		// render triangles
 		pipeline.Draw(itlist);
 
@@ -130,4 +139,5 @@ private:
 	float lpos_x = 0.0f;
 	float lpos_y = 0.0f;
 	float lpos_z = 0.6f;
+	float phi = 0.0f;
 };
